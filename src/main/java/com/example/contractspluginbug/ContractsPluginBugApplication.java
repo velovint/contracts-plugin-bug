b7b78f1ce504a1b9dda6504c1d6b22118001957c
Vitali Yakavenka <vitali.yakavenka@kroger.com>
2018-12-05 11:08:33 -0800

    Init commit

---
+++ src/main/java/com/example/contractspluginbug/ContractsPluginBugApplication.java
@@ -0,0 +1,12 @@
+package com.example.contractspluginbug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractsPluginBugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractsPluginBugApplication.class, args);
+	}
+}
